dbd0ca5073e834fe816779d397b3399683576777
huiitre <yanisdeluca69@gmail.com>
2025-12-27 22:43:27 +0100

    ajout config openapi pour triage des endpoints sur swagger ui

---
+++ src/main/java/fr/huiitre/tools/config/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package fr.huiitre.tools.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    tags = {
+        @Tag(name = "Core - Auth"),
+        @Tag(name = "Core - Module"),
+        @Tag(name = "Core - Test")
+    }
+)
+public class OpenApiConfig {
+}
